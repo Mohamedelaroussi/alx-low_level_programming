b40048ed06f9164feb796984fa6b529b4c938fd2
Mohamedelaroussi <midmoha93@gmail.com>
2023-06-15 17:53:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <sdtio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: A C program that prints whit printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art.\n");
+	return  (0);
+
+}
