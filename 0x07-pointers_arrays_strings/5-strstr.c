90a2cdb6c0b78148568b1fc12c31107bb76066d9
Mohamedelaroussi <midmoha93@gmail.com>
2023-07-03 11:26:56 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,7 +5,6 @@
  * _strstr - function that locates a substring.
  * @haystack: source string to search through.
  * @needle: string to search for.
- *
  * Return: return beginning of located substring or NULL.
  */
 char *_strstr(char *haystack, char *needle)
