44391049f619e422831692b8ef35fdfe7214da8c
Mohamedelaroussi <midmoha93@gmail.com>
2023-08-08 04:35:21 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,40 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
 /**
- * create_file - function that will create a new file and fill with with some
- * content.
- * @filename: name of the file to create.
- * @text_content: text to add to the new file.
+ * create_file - function that creates a file
+ * permission rw-------
+ * If the file already exists, do not change the permissions.
+ * if the file already exists, truncate it
  *
- * Return: Always 1 on scucess, -1 on Failure
+ * @filename: name of the file to create
+ * if filename is NULL return -1
+ * @text_content: NULL terminated string to write to the file
+ * if text_content is NULL create an empty file
+ *
+ * Return: 1 on succes
+ * -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int err, len, fd;
+	int fd, num_written, len = 0;
 
-	err = len = fd = 0;
-	if (!filename)
+	if (filename == 0)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-		if (fd < 0)
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+		num_written = write(fd, text_content, len);
+
+		if (num_written == -1)
 			return (-1);
-	while (text_content && text_content[len])
-		len++;
-
-	err = write(fd, text_content, len);
-	if (err < 0)
-		return (-1);
+	}
 	close(fd);
 	return (1);
 }
-
-
